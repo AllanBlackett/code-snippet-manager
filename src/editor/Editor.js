30086404bf7800622c4ce8ee52cb39ed40d71027
Allan <Allanblackett@gmail.com>
2020-06-24 11:46:31 -0400

    working on react-quill async functionality

--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -19,10 +19,24 @@ class EditorComponent extends Component {
     const { classes } = this.props;
 
     return(
+      // Whatever the text: '', is from this.state object is going to be the value property of react-quill.
+      // this.updateBody is going to be a function that is going to be asyncronous
+        // that's going to set some state, and once the state updates, call the update function.   
     <div className={classes.editorContainer}>
-      <ReactQuill></ReactQuill>
-    </div>);
+      <ReactQuill
+        value={this.state.text} 
+        onChange={this.updateBody}>
+
+      </ReactQuill>
+    </div>
+    );
   }
+  update = async (val) => {
+    // Wait for the state to update
+    await this.setState({ text: val });
+    // once state is update then call the update() method
+    this.update();
+  };
 }
 
 export default withStyles(styles)(EditorComponent);
