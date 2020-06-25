090254b370597a0756df602af85afff7037c90ae
Allan <Allanblackett@gmail.com>
2020-06-25 05:51:45 -0400

    Added Component for snippet selections

--- src/sidebarElement/SbElement.js
+++ src/sidebarElement/SbElement.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-// import ListItem from '@material-ui/core/ListItem';
-// import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import { ListItemText } from '@material-ui/core';
+//! import ListItemText from '@material-ui/core/ListItemText';
 // import DeleteIcon from '@material-ui/icons/Delete';
 // import { removeHTMLTags } from '../helpers';
 
 class SidebarElementComponent extends Component {
   render() {
-    return(<div>Hello from the sidebar element</div>);
+    
+    const { _index, _snippet, classes, selectedSnippetIndex } = this.props;
+
+    return(
+    <div key={_index}>
+      <ListItem
+        classname={classes.listItem}
+        // If selected snippet index = true, then it will be higlighted
+        selected={selectedSnippetIndex === _index}
+        alignItems='flex-start'>  
+          <div 
+            className={classes.textSection}
+            onClick={() => this.selecSnippet(_snippet, _index)}>
+              <ListItemText
+                primary={_snippet.title}
+                secondary={_snippet.body.substring(0, 30) + '...'}>
+              </ListItemText>
+            </div>
+      </ListItem>
+    </div>
+    );
   }
+
 }
 
 export default withStyles(styles)(SidebarElementComponent);
